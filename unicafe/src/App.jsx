945afde4be7403c72b05c219d5929ebb8345ec5c
mmartbar <57923226+mmartbar@users.noreply.github.com>
2024-06-26 13:35:18 +0200

    Update App.jsx 1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,11 +1,10 @@
-//Version 1.8
+//Version 1.9
 import { useState } from 'react'
 
-const Statistics = (props) => {
+const Statistics = (props) => {  
   return (
     <>
       {props.text} {props.stats} {props.symbol}
-      <br></br>
     </>
   )
 }
@@ -19,6 +18,62 @@ const App = () => {
   const avg=((good*1)+(neutral*0)+(bad*-1))/total
   const positive=(good*100)/total
 
+  const parameters = [
+    {
+      text: 'good',
+      stats: good,
+      symbol: undefined
+    },
+    {
+      text: 'neutral',
+      stats: neutral,
+      symbol: undefined
+    },
+    {
+      text: 'bad',
+      stats: bad,
+      symbol: undefined
+    },
+    {
+      text: 'all',
+      stats: total,
+      symbol: undefined
+    },
+    {
+      text: 'average',
+      stats: avg,
+      symbol: undefined
+    },
+    {
+      text: 'positive',
+      stats: positive,
+      symbol: '%'
+    },
+    {
+      text: undefined,
+      stats: undefined,
+      symbol: undefined
+    }
+  ]
+  
+  if ( good==0 && neutral==0 && bad==0) {
+    parameters[0].text=undefined
+    parameters[0].stats=undefined
+    parameters[1].text=undefined
+    parameters[1].stats=undefined
+    parameters[2].text=undefined
+    parameters[2].stats=undefined
+    parameters[3].text=undefined
+    parameters[3].stats=undefined
+    parameters[4].text=undefined
+    parameters[4].stats=undefined
+    parameters[5].text=undefined
+    parameters[5].stats=undefined
+    parameters[5].symbol=undefined
+    parameters[6].text='No feedback given'
+    parameters[6].stats=undefined
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,12 +82,13 @@ const App = () => {
       <button onClick={() => setBad(bad+1)}>bad</button>
 
       <h1>statistics</h1>
-      <Statistics text='good' stats={good}/>
-      <Statistics text='neutral' stats={neutral}/>
-      <Statistics text='bad' stats={bad}/>
-      <Statistics text='all' stats={total}/>
-      <Statistics text='average' stats={avg}/>
-      <Statistics text='positive' stats={positive} symbol='%'/>
+      <Statistics text={parameters[0].text} stats={parameters[0].stats}/>
+      <Statistics text={parameters[1].text} stats={parameters[1].stats}/>
+      <Statistics text={parameters[2].text} stats={parameters[2].stats}/>
+      <Statistics text={parameters[3].text} stats={parameters[3].stats}/>
+      <Statistics text={parameters[4].text} stats={parameters[4].stats}/>
+      <Statistics text={parameters[5].text} stats={parameters[5].stats} symbol={parameters[5].symbol}/>
+      <Statistics text={parameters[6].text} stats={parameters[6].stats}/>
     </div>
   )
 }
